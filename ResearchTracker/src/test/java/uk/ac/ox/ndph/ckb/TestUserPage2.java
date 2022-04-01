45bac7b03c84721a889110528f231c21e6600705
rjngshn <87362844+rjngshn@users.noreply.github.com>
2022-04-01 14:48:25 +0100

    Add files via upload

---
+++ ResearchTracker/src/test/java/uk/ac/ox/ndph/ckb/TestUserPage2.java
@@ -0,0 +1,44 @@
+package uk.ac.ox.ndph.ckb;
+
+import org.apache.wicket.protocol.http.WebSession;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Simple test using the WicketTester
+ */
+public class TestUserPage2
+{
+    private WicketTester tester;
+
+    @BeforeEach
+    public void setUp()
+    {
+        tester = new WicketTester(new RTApplication());
+    }
+// https://stackoverflow.com/questions/70258881/wicket-9-unit-testing-mockito-httpsession
+    @Test
+    public void userPage2RendersSuccessfully()
+    {
+        System.out.println("Testing userPage2RendersSuccessfully ");
+        System.out.println("Getting AppSession");
+        WebSession webSession = (WebSession)tester.getSession();
+        AppSession appSession = (AppSession)webSession;
+        appSession.setUserName("Test user 1");
+        System.out.println("Uesr name in session:" + appSession.getUserName());
+        //start and render the test page
+        tester.startPage(UserPage2.class);
+        //assert rendered page class
+        tester.assertRenderedPage(UserPage2.class);
+        tester.assertModelValue("txtUserName", "Test user 1");
+        webSession = (WebSession)tester.getSession();
+        appSession = (AppSession)webSession;
+        appSession.setUserName("Test user 2");
+        tester.startPage(UserPage2.class);
+        //assert rendered page class
+        tester.assertRenderedPage(UserPage2.class);
+        tester.assertModelValue("txtUserName", "Test user 2");
+        System.out.println("Complete testing userPage2RendersSuccessfully ");
+    }
+}
